305234cd6bca0a56cd675e46f2fe970b78177647
Thibaut SORIANO <st405262@etu.unice.fr>
2016-07-06 11:43:49 +0200

    YU KAIWEN

--- src/main/java/gui/DiasGrosFilsDePuteWindow.java
+++ src/main/java/gui/DiasGrosFilsDePuteWindow.java
@@ -50,50 +50,50 @@ public class DiasGrosFilsDePuteWindow {
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 85, 536, 349);
+		panel.setBounds(0, 20, 584, 380);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JCheckBox checkBox = new JCheckBox("Separator line above footer");
 		checkBox.setSelected(true);
-		checkBox.setBounds(242, 166, 189, 23);
+		checkBox.setBounds(284, 143, 189, 23);
 		panel.add(checkBox);
 		
 		JCheckBox checkBox_1 = new JCheckBox("Separator line below header");
 		checkBox_1.setSelected(true);
-		checkBox_1.setBounds(242, 94, 189, 23);
+		checkBox_1.setBounds(284, 71, 189, 23);
 		panel.add(checkBox_1);
 		
 		JSeparator separator = new JSeparator();
-		separator.setBounds(51, 80, 397, 2);
+		separator.setBounds(93, 57, 397, 2);
 		panel.add(separator);
 		
 		JSeparator separator_1 = new JSeparator();
-		separator_1.setBounds(51, 134, 397, 2);
+		separator_1.setBounds(93, 111, 397, 2);
 		panel.add(separator_1);
 		
 		JCheckBox checkBox_2 = new JCheckBox("Header");
-		checkBox_2.setBounds(51, 94, 189, 23);
+		checkBox_2.setBounds(93, 71, 189, 23);
 		panel.add(checkBox_2);
 		
 		JCheckBox checkBox_3 = new JCheckBox("Footer");
-		checkBox_3.setBounds(51, 166, 189, 23);
+		checkBox_3.setBounds(93, 143, 189, 23);
 		panel.add(checkBox_3);
 		
 		JLabel lblTextBelowTitle = new JLabel("Text below title");
-		lblTextBelowTitle.setBounds(51, 233, 159, 19);
+		lblTextBelowTitle.setBounds(93, 210, 159, 19);
 		panel.add(lblTextBelowTitle);
 		
 		JSeparator separator_2 = new JSeparator();
-		separator_2.setBounds(51, 212, 397, 2);
+		separator_2.setBounds(93, 189, 397, 2);
 		panel.add(separator_2);
 		
 		JSeparator separator_3 = new JSeparator();
-		separator_3.setBounds(51, 320, 397, 2);
+		separator_3.setBounds(93, 297, 397, 2);
 		panel.add(separator_3);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(196, 232, 243, 71);
+		scrollPane.setBounds(238, 209, 243, 71);
 		panel.add(scrollPane);
 		
 		
