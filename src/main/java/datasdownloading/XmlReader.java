4b5e8486e0f00bc179edcdc17a4e43f988c67044
Thibaut SORIANO <st405262@etu.unice.fr>
2016-07-14 15:58:46 +0200

    correction d'une error pour le timestamp (*1000)

--- src/main/java/datasdownloading/XmlReader.java
+++ src/main/java/datasdownloading/XmlReader.java
@@ -8,7 +8,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
@@ -16,6 +18,7 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import main.java.datasdownloading.entities.Campaign;
 import main.java.datasdownloading.entities.CampaignHeader;
 import main.java.datasdownloading.entities.CampaignStatus;
 
@@ -23,6 +26,8 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
 public class XmlReader {
+	
+	private Map<String, String> placementsNames = new HashMap<>();
 
 	public static void main(String args[]) {
 
@@ -134,17 +139,19 @@ public class XmlReader {
 
 					Element eElement = (Element) nNode;
 
+					String campaignID = eElement.getElementsByTagName("campaignID").item(0).getTextContent();
 					String campaignName = eElement.getElementsByTagName("name").item(0).getTextContent();
 					String clientName = eElement.getElementsByTagName("clientName").item(0).getTextContent();
 					CampaignStatus campaignStatus = CampaignStatus
 							.valueOf(eElement.getElementsByTagName("status").item(0).getTextContent().toUpperCase());
 					Date creationDate = new Date(
-							Integer.parseInt(eElement.getElementsByTagName("creationTS").item(0).getTextContent()));
+							Long.parseLong(eElement.getElementsByTagName("creationTS").item(0).getTextContent()) * 1000);
 					Date startDate = new Date(
-							Integer.parseInt(eElement.getElementsByTagName("startTS").item(0).getTextContent()));
+							Long.parseLong(eElement.getElementsByTagName("startTS").item(0).getTextContent()) * 1000);
 					Date endDate = new Date(
-							Integer.parseInt(eElement.getElementsByTagName("endTS").item(0).getTextContent()));
+							Long.parseLong(eElement.getElementsByTagName("endTS").item(0).getTextContent()) * 1000);
 
+					System.out.println("Campaign ID : " + campaignID);
 					System.out.println("Campaign name : " + campaignName);
 					System.out.println("Client name : " + clientName);
 					System.out.println("Status : " + campaignStatus);
@@ -152,7 +159,7 @@ public class XmlReader {
 					System.out.println("Start TS : " + startDate);
 					System.out.println("End TS : " + endDate);
 
-					headerList.add(new CampaignHeader(campaignName, clientName, campaignStatus, creationDate, startDate,
+					headerList.add(new CampaignHeader(campaignID, campaignName, clientName, campaignStatus, creationDate, startDate,
 							endDate));
 
 				}
@@ -163,4 +170,60 @@ public class XmlReader {
 
 		return headerList;
 	}
+	
+	public void fillMapPlacementsNames(String xmlPlacementList) {		
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(new InputSource(new ByteArrayInputStream(xmlPlacementList.getBytes("utf-8"))));
+
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("placement");
+
+			for (int i = 0; i < nList.getLength(); i++) {
+
+				Node nNode = nList.item(i);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+	
+					Element eElement = (Element) nNode;
+					placementsNames.put(eElement.getElementsByTagName("placementID").item(0).getTextContent(), eElement.getElementsByTagName("name").item(0).getTextContent());
+					System.out.println("placement ID = " + eElement.getElementsByTagName("placementID").item(0).getTextContent());
+					System.out.print("placementName = " + eElement.getElementsByTagName("name").item(0).getTextContent());
+				}
+			}
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Campaign getCampaign(String campaignID, String xmlCampaignDatas, String xmlPlacementList) {
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(new InputSource(new ByteArrayInputStream(xmlPlacementList.getBytes("utf-8"))));
+
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("placement");
+
+			for (int i = 0; i < nList.getLength(); i++) {
+
+				Node nNode = nList.item(i);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+	
+					Element eElement = (Element) nNode;
+					placementsNames.put(eElement.getElementsByTagName("placementID").item(0).getTextContent(), eElement.getElementsByTagName("name").item(0).getTextContent());
+					System.out.println("placement ID = " + eElement.getElementsByTagName("placementID").item(0).getTextContent());
+					System.out.print("placementName = " + eElement.getElementsByTagName("name").item(0).getTextContent());
+				}
+			}
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	
 }
