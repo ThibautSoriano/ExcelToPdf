5f95b5eeeba062bd899284d3f5cfd9c8f4d64359
= <cappe@polytech.unice.fr>
2016-07-14 12:23:42 +0200

    Methodes http download pas encore fonctionelles

--- src/main/java/datasdownloading/HttpDownload.java
+++ src/main/java/datasdownloading/HttpDownload.java
@@ -6,11 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 import javax.net.ssl.HttpsURLConnection;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -18,22 +20,42 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 public class HttpDownload {
+    
+    
+    private String userName;
+    
+    private String password;
+    
+    private String sessionId;
+    
+    
+    public HttpDownload(String userName,String password){
+        this.userName = userName;
+        this.password = password;
+    
+        HttpMessage m = login(this.userName, this.password);
+        
+        if (!m.isOk()) {
+            System.out.println(m.getErrorMessage());
+        }
+    }
+    
 
+    public HttpDownload(){
+        this("zburi_owner", "ad12dac");
+    }
+    
+    
     public static void main(String[] args) throws Exception {
-
         HttpDownload http = new HttpDownload();
-
-        System.out.println("Testing 1 - Send Http GET request");
-        http.sendGet("zburi_owner", "ad12dac");
-
     }
 
-    // HTTP GET request
-    private void sendGet(String loginName, String password) throws Exception {
-
-        String url = "http://gdeapi.gemius.com/OpenSession.php?ignoreEmptyParams=Y&login="
-                + loginName + "&passwd=" + password;
-
+    
+    
+    
+    
+    
+    private HttpMessage sendGet(String url) {
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(url);
 
@@ -41,16 +63,58 @@ public class HttpDownload {
             HttpResponse response = client.execute(request);
             HttpEntity entity = response.getEntity();
 
-            //
             // Read the contents of an entity and return it as a String.
-            //
             String content = EntityUtils.toString(entity);
+            
             System.out.println(content);
-        } catch (IOException e) {
-            e.printStackTrace();
+        //TODO fill session id
+            return new HttpMessage(true, "OK",content);
         }
-
-
+         catch (UnknownHostException e) {
+             return new HttpMessage(false, "No internet","");
+        } catch (ClientProtocolException e) {
+            return new HttpMessage(false, e.getMessage(),"");
+        } catch (IOException e) {
+            return new HttpMessage(false, e.getMessage(),"");
+        }
     }
+    
+    
+    /**
+     *  Tries to log in with the distant host gdeapi.gemius.com, if succes fills sessionId
+     * @param loginName
+     * @param password
+     * @return a message containing infos about the success of the login 
+     */
+    private HttpMessage login(String loginName, String password) {
+        
+        String url = "http://gdeapi.gemius.com/OpenSession.php?ignoreEmptyParams=Y&login="
+                + loginName + "&passwd=" + password;
+
+        HttpMessage m = sendGet(url);
+
+        if (m.isOk())
+            ; 
+        
+        
+        return m;
+    }
+    
+    
+    public List<Float>() getCampaignHeaders(){
+        
+        
+        
+        String url = "http://gdeapi.gemius.com/GetCampaignsList.php?ignoreEmptyParams=Y&sessionID="+sessionId;
+        
+        HttpMessage m = sendGet(url);
+        
+        if (!m.isOk())
+            System.out.println(m.getErrorMessage());
+        return null;
+        
+        
+    }
+    
 
 }
